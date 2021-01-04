110f4a634dfbc2ddee7e67b0b103bb9e4a102a47
Gwladys Sette <gwladys.sette@etudiant.univ-lr.fr>
2021-01-04 18:03:05 +0100

    tp2 dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'admin-miaw';
+  title = 'Dashboard';
+  user = {
+    name: 'Jean'
+  };
+  skills = [
+    {
+      name: 'Développement WordPress',
+      logo: 'assets/img/wordpress.png'
+    },
+    {
+      name: 'Développement Front-end',
+      logo: null
+    },
+    {
+      name: 'Webdesign',
+      logo: null
+    }
+  ];
+  clients = [
+    {
+      name: 'Martin',
+    },
+    {
+      name: 'Robert',
+    },
+    {
+      name: 'Bastien',
+    }
+  ];
 }
